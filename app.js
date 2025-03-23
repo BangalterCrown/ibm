0d461845e5a3405f7758834d238ae69785756a9b
BangalterCrown <husseinpedraza@icloud.com>
2025-03-22 22:53:13 -0600

    Practica Galeria de Fotos

---
+++ app.js
@@ -0,0 +1,8 @@
+$prev.addEventListener (
+    'click',
+    () => {
+        const items = document.querySelectorAll('.item');
+        document.querySelector('.slide').prepend (items [items. length - 1]);
+    },
+);
+    
